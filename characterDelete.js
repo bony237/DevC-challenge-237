f53ec71cdb365dba08ea688f454bfe86124d0bb7
bony237 <54316040+bony237@users.noreply.github.com>
2020-02-13 08:03:25 +0100

    problem 03

---
+++ characterDelete.js
@@ -0,0 +1,13 @@
+/*
+        Delete the first and the last character of a string
+
+*/
+
+// Proposition
+function characterDel(theString) {
+    return theString.slice(1,-1);
+}
+
+
+// Exemple
+console.log(characterDel('Ceci est une phrase'));    // --> result : 'eci est une phras'
